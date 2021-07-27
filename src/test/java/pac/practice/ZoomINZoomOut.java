f04da007c13f66ee1179050100c6724e2f1d6b8e
pvpatil5 <pvpatil123@yahoo.com>
2021-07-27 17:01:38 +0530

    New

--- src/test/java/pac/practice/ZoomINZoomOut.java
+++ src/test/java/pac/practice/ZoomINZoomOut.java
@@ -1,5 +1,6 @@
 package pac.practice;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -9,11 +10,28 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 
 public class ZoomINZoomOut {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-	
+
+		AppiumDriverLocalService	 server = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+				.withArgument(GeneralServerFlag.SESSION_OVERRIDE)
+				.usingPort(4723)
+				.withLogFile(new File("../AppiumSDET17/Logs.txt")));
+
+
+		server.start();
+
+
+
+
+
+
+
 		DesiredCapabilities dc = new DesiredCapabilities();
 		// Common DC (Android or IOS)
 		dc.setCapability("deviceName", "Redmi");		
@@ -32,21 +50,23 @@ public class ZoomINZoomOut {
 		AndroidDriver driver = new AndroidDriver(url, dc);
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+
 		driver.findElement(By.id("com.davemorrissey.labs.subscaleview.sample:id/basicFeatures")).click();
-		
+
 		WebElement img = driver.findElement(By.id("com.davemorrissey.labs.subscaleview.sample:id/imageView"));
-		
+
 		driver.zoom(img);
-		
+
 		Thread.sleep(3000);
-		
+
 		driver.pinch(img);
-		
-		
-		
+
+
+
 		driver.hideKeyboard();
-		
+
+
+		server.stop();
 	}
 
 }
